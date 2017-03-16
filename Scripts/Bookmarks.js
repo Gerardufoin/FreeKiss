3434667f0f476807869ae66b29b427b78c9e2cbe
Thibault Duval <gerardufoin2@hotmail.com>
2017-03-17 00:07:51 +0100

    Get the images from the bookmarks page; Start to change the layout of the bookmarks page

--- Scripts/Bookmarks.js
+++ Scripts/Bookmarks.js
@@ -1,5 +1,24 @@
 "use strict";
 
+// The data of the images is processed by JQuery Tooltip and removed. So we need to get them before it happens using mutations.
+var observer = new MutationObserver(function(mutations) {
+  mutations.forEach(function(mutation) {
+  	if (mutation.oldValue != null && $(mutation.oldValue).length == 3) {
+  		var node = $(mutation.oldValue);
+  		$(mutation.target).prepend('<a href="' + node.find("a").attr("href") + '"><img src="' + $(node[0]).attr("src") + '" class="fk-bookmarkImage" /></a>');
+  	}
+  });    
+});
+observer.observe(document,
+	{
+		attributes: true,
+		attributeOldValue: true,
+		childList: false,
+		characterData: false,
+		subtree: true,
+		attributeFilter: [ "title" ]
+	});
+
 $(document).ready(function () {
 
 	// Guidelines Changes
@@ -12,12 +31,20 @@ $(document).ready(function () {
 		$("#rightside .barContent").toggleClass("fk-hide");
 	});
 
-	/*var mangas = {};
+	// Bookmarks layout
+	$(".listing tr:first-child").remove();
+	$(".listing tr").addClass("fk-bookmarkRow");
+	$(".fk-bookmarkRow td:nth-child(1)").addClass("fk-bookmarkTitle");
+	$(".fk-bookmarkRow td:nth-child(2)").addClass("fk-bookmarkChapter");
+	$(".fk-bookmarkRow td:nth-child(3), .fk-bookmarkRow td:nth-child(4)").addClass("fk-bookmarkStatus");
+
+	// Bookmarks infos
+	var mangas = {};
 	$(".listing tr:not(:first-child)").each(function() {
 		var m = {
 			name: $(this).find("td:eq(0) a").text()
 		};
 		mangas[$(this).find("td:eq(3) a").attr("mid")] = m;
-		console.log(mangas);
-	});*/
+		//console.log($(this).find("td:eq(0)"));
+	});
 });
