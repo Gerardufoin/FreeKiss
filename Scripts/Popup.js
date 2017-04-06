cb428b70e03abc75dc3ac2d202fd249135157064
Thibault Duval <gerardufoin2@hotmail.com>
2017-04-06 22:25:06 +0200

    Add menu display for quick page resize

--- Scripts/Popup.js
+++ Scripts/Popup.js
@@ -2,16 +2,32 @@
 
 $(document).ready(function() {
 
+	//chrome.storage.local.remove("fk-options");
 	chrome.storage.local.get("fk-options", function(options) {
 		options = options['fk-options'];
 		if (options == null) {
 			options = {};
 		}
 
-		if (options.hasOwnProperty('disable') && options.disable)
+		if (!options.hasOwnProperty("maxPageWidth")) options.maxPageWidth = 1800;
+		if (!options.hasOwnProperty("maxDoublePageWidth")) options.maxDoublePageWidth = 1800;
+		if (!options.hasOwnProperty("minPageWidth")) options.minPageWidth = 1800;
+		if (!options.hasOwnProperty("minDoublePageWidth")) options.minDoublePageWidth = 1800;
+		if (options.hasOwnProperty("disable") && options.disable)
 		{
 			$("#disable").addClass("active").text("Enable FreeKiss");
 		}
+
+		$(".slider[name=maxPageWidth]").val(options.maxPageWidth).next().text(options.maxPageWidth);
+		$(".slider[name=maxDoublePageWidth]").val(options.maxDoublePageWidth).next().text(options.maxDoublePageWidth);
+		$(".slider[name=minPageWidth]").val(options.minPageWidth).next().text(options.minPageWidth);
+		$(".slider[name=minDoublePageWidth]").val(options.minDoublePageWidth).next().text(options.minDoublePageWidth);
+		
+		$(".slider").on("input", function() {
+			$(this).next().text($(this).val());
+			options[$(this).attr("name")] = $(this).val();
+			chrome.storage.local.set({"fk-options": options});
+		});
 
 		$("#disable").click(function() {
 			$(this).toggleClass("active");
