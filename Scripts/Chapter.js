63e1dc61b822df32224ae4e124c08e4619ac7311
Thibault Duval <gerardufoin2@hotmail.com>
2017-11-14 22:05:33 +0100

    Add resize range; Remove useless image operations; Optimize resize speed at load time

--- Scripts/Chapter.js
+++ Scripts/Chapter.js
@@ -1,18 +1,16 @@
 "use strict";
 
-// Variable used to get images height/width. Created here to avoid spamming "new"
-var fk_test_image = new Image();
+// Constant setting the acceptable range variation on the images resize. This is here to avoid unnecessary drop of images quality. (For exemple a max-size of 800px on a 802px image)
+var SIZE_RANGE = 50;
 
 function Chapter() {
+	// Sync the manager is required on the page, we sync the Management
 	if (FreeKiss.Options.get("chapterManager")) {
 		Management.Synchronize();
 	}
-	Mutations();
-}
 
-function Mutations() {
 	let injectManager = !FreeKiss.Options.get("chapterManager");
-	// Mutation are used to get the images as they are added to the page to resize them
+	// Mutation are used to get the images as they are added to the page to resize them and add the manager during the page load (because I'm picky)
 	new MutationObserver(function(mutations) {
 		// We inject the manager if #navsubbar has been added to the page
 		if (!injectManager && document.getElementById("navsubbar") != undefined) {
@@ -26,69 +24,69 @@ function Mutations() {
 			// All images are in the #divImage element
 			if (mutation.target.id == "divImage") {
 				mutation.addedNodes.forEach(function(node) {
-					// If the node is an element (1) we apply the onLoad (could be a text (3))
-					if (node.nodeType == 1) {
-						$(node).find("img").on('load', function() {
-							FK_ApplyResizeOptions($(this));
-						});
-					}
-				});
-			}
-
-			// We get the manga id from the page script
-			if ($(mutation.target).prop("tagName") == "SCRIPT") {
-				mutation.addedNodes.forEach(function(node) {
-					let mid = $(node).text().match(/mangaID=(\d+)/);
-					if (mid != null) {
-						$(".fk-mAdd").attr("mid", mid[1]);
+					if (node.tagName == "P") {
+						$.map($(node).find("img"), (img, i) => { FK_ApplyResizeOptions($(img)); });
 					}
 				});
 			}
 		});
 	}).observe(document, {childList: true, subtree: true});
+
+	$(document).ready(function() {
+		if (FreeKiss.Options.get("chapterManager")) {
+			// We get the manga id for the manager from one of the scripts
+			$.map($("script"), function(node, i) {
+				let mid = $(node).text().match(/mangaID=(\d+)/);
+				if (mid != null) {
+					$(".fk-mAdd").attr("mid", mid[1]);
+				}
+			});
+		}
+	});
 }
 
 FreeKiss.init(Chapter);
 
 // Resize of the image depending on the options and if it's a double-page or not
 function FK_ApplyResizeOptions(img) {
-	fk_test_image.src = img.attr("src");
-
-	if (!FK_IsDoublePage(fk_test_image)) {
+	if (!$(img)[0].naturalWidth) {
+		setTimeout(() => { FK_ApplyResizeOptions(img); }, 50);
+		return;
+	}
+	if (!FK_IsDoublePage($(img)[0])) {
 		if (FreeKiss.Options.isSet("maxPageWidth") && FreeKiss.Options.get("maxDisable") === false) {
-			img.css("max-width", parseInt(FreeKiss.Options.get("maxPageWidth")));
+			img.css("max-width", parseInt(FreeKiss.Options.get("maxPageWidth")) + SIZE_RANGE);
 		}
 		if (FreeKiss.Options.isSet("minPageWidth") && FreeKiss.Options.get("minDisable") === false) {
-			img.css("min-width", parseInt(FreeKiss.Options.get("minPageWidth")));
+			img.css("min-width", parseInt(FreeKiss.Options.get("minPageWidth")) - SIZE_RANGE);
 		}
 	} else {
 		if (FreeKiss.Options.isSet("maxDoublePageWidth") && FreeKiss.Options.get("maxDisable") === false) {
-			img.css("max-width", parseInt(FreeKiss.Options.get("maxDoublePageWidth")));
+			img.css("max-width", parseInt(FreeKiss.Options.get("maxDoublePageWidth")) + SIZE_RANGE);
 		}
 		if (FreeKiss.Options.isSet("minDoublePageWidth") && FreeKiss.Options.get("minDisable") === false) {
-			img.css("min-width", parseInt(FreeKiss.Options.get("minDoublePageWidth")));
+			img.css("min-width", parseInt(FreeKiss.Options.get("minDoublePageWidth")) - SIZE_RANGE);
 		}
 	}
 }
 
 // This function is called by Popup.js when the options are changed
 function FK_PageResize(attribute, value, maxDisable, minDisable) {
-
 	$("#divImage p > img").each(function() {
-		fk_test_image.src = $(this).attr("src");
+		let isDouble = FK_IsDoublePage($(this)[0]);
 
 		switch(attribute) {
 			case "maxPageWidth":
-				if (!FK_IsDoublePage(fk_test_image) && !maxDisable) $(this).css("max-width", value);
+				if (!isDouble && !maxDisable) $(this).css("max-width", value + SIZE_RANGE);
 				break;
 			case "maxDoublePageWidth":
-				if (FK_IsDoublePage(fk_test_image) && !maxDisable) $(this).css("max-width", value);
+				if (isDouble && !maxDisable) $(this).css("max-width", value + SIZE_RANGE);
 				break;
 			case "minPageWidth":
-				if (!FK_IsDoublePage(fk_test_image) && !minDisable) $(this).css("min-width", value);
+				if (!isDouble && !minDisable) $(this).css("min-width", value - SIZE_RANGE);
 				break;
 			case "minDoublePageWidth":
-				if (FK_IsDoublePage(fk_test_image) && !minDisable) $(this).css("min-width", value);
+				if (isDouble && !minDisable) $(this).css("min-width", value - SIZE_RANGE);
 				break;
 		}
 	});
